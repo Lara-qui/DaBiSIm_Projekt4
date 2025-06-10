f5591da1cbc209d555b50da309247a09059c58a4
CarlaKolumnaDaBiSim <carla.stingl@gmail.com>
2025-06-10 13:27:46 +0200

    Update protein_viewer_streamlit.py

--- protein_viewer_streamlit.py
+++ protein_viewer_streamlit.py
@@ -106,7 +106,7 @@ def visualize_protein_3d(protein, selected_chains, selected_elements, color_mode
                 atoms_by_group[key].append(atom.coord)
 
     element_colors = {
-        'H': 'white', 'C': 'darkolivegreen', 'N': 'blue', 'O': 'red', 'S': 'yellow',
+        'H': 'white', 'C': 'green', 'N': 'blue', 'O': 'red', 'S': 'yellow',
         'P': 'orange', 'Fe': 'darkgray', 'Zn': 'purple', 'Cl': 'green',
         'Na': 'deepskyblue', 'K': 'violet', 'Ca': 'limegreen', 'Mg': 'teal'
     }
